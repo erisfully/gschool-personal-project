86f71e031fa77e6d4627006287ee883bc392a7f9
Ashley <a.clarkem@gmail.com>
2014-07-24 16:02:39 -0600

    register fields added

--- app.rb
+++ app.rb
@@ -7,6 +7,12 @@ class App < Sinatra::Application
   enable :sessions
   use Rack::Flash
 
+  def initialize
+    super
+    @database_connection = GschoolDatabaseConnection::DatabaseConnection.establish(ENV["RACK_ENV"])
+  end
+
+
   get "/" do
     erb :root
   end
